aa24e597f4a968710da9bb969a9505232ed18c07
Nikita Mandrykin <189896017+b1sted@users.noreply.github.com>
2026-07-30 01:33:26 +0300

    feat(annotations): add PageInfo, PagePath annotations and AnnotationUtils helper

---
+++ src/test/java/ru/basted/basteduitests/annotations/PageInfo.java
@@ -0,0 +1,13 @@
+package ru.basted.basteduitests.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface PageInfo {
+    String siteEntity();
+    String title();
+}
