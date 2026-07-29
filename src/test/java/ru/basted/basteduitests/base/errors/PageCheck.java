fe07afbaf6b5915b2a9cc6fd7578b9e9a3e903fb
Nikita Mandrykin <189896017+b1sted@users.noreply.github.com>
2026-07-30 01:34:01 +0300

    feat(base.errors): add Check contract, PageState and PageCheck

---
+++ src/test/java/ru/basted/basteduitests/base/errors/PageCheck.java
@@ -0,0 +1,23 @@
+package ru.basted.basteduitests.base.errors;
+
+public enum PageCheck implements Check {
+    URL("%s -> %s -> Параметр: URL", "Ожидался URL '%s', но получен '%s'");
+
+    private final String context;
+    private final String failMessageTemplate;
+
+    PageCheck(String context, String failMessageTemplate) {
+        this.context = context;
+        this.failMessageTemplate = failMessageTemplate;
+    }
+
+    @Override
+    public String context(PageState pageState) {
+        return context.formatted(pageState.siteEntity(), pageState.pageTitle());
+    }
+
+    @Override
+    public String failMessage(Object expected, Object actual) {
+        return failMessageTemplate.formatted(expected, actual);
+    }
+}
