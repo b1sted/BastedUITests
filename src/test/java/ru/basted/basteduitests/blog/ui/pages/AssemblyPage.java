84529abdf144c6d39b0e8c1d4e500a308ce653da
Nikita Mandrykin <189896017+b1sted@users.noreply.github.com>
2026-08-02 23:11:41 +0300

    refactor(blog.ui.pages): implement HasArticles on BlogPage, AssemblyPage

--- src/test/java/ru/basted/basteduitests/blog/ui/pages/AssemblyPage.java
+++ src/test/java/ru/basted/basteduitests/blog/ui/pages/AssemblyPage.java
@@ -6,11 +6,12 @@ import org.openqa.selenium.WebDriver;
 import ru.basted.basteduitests.annotations.PageInfo;
 import ru.basted.basteduitests.annotations.PagePath;
 import ru.basted.basteduitests.base.BasePage;
+import ru.basted.basteduitests.base.capabilities.HasArticles;
 import ru.basted.basteduitests.base.capabilities.HasBackButton;
 
 @PageInfo(siteEntity = "Блог", name = "Изучаем Assembly NASM")
 @PagePath("/assembly")
-public final class AssemblyPage extends BasePage<AssemblyPage> implements HasBackButton {
+public final class AssemblyPage extends BasePage<AssemblyPage> implements HasArticles, HasBackButton {
     private final By expandButtonLocator = By.cssSelector("button[class*='assembly']");
 
     public AssemblyPage(WebDriver webDriver) {
--- src/test/java/ru/basted/basteduitests/blog/ui/pages/BlogPage.java
+++ src/test/java/ru/basted/basteduitests/blog/ui/pages/BlogPage.java
@@ -6,11 +6,12 @@ import org.openqa.selenium.WebDriver;
 import ru.basted.basteduitests.annotations.PageInfo;
 import ru.basted.basteduitests.annotations.PagePath;
 import ru.basted.basteduitests.base.BasePage;
+import ru.basted.basteduitests.base.capabilities.HasArticles;
 import ru.basted.basteduitests.base.capabilities.HasBackButton;
 
 @PageInfo(siteEntity = "Блог", name = "Блог")
 @PagePath("/blog")
-public final class BlogPage extends BasePage<BlogPage> implements HasBackButton {
+public final class BlogPage extends BasePage<BlogPage> implements HasArticles, HasBackButton {
     private final By sectionTitleLocator = By.cssSelector("h2[class*='section']");
 
     public BlogPage(WebDriver webDriver) {
