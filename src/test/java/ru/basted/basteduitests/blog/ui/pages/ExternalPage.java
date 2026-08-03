a49cd73fb3a413462fdefc0fa862349f70a45f57
Nikita Mandrykin <189896017+b1sted@users.noreply.github.com>
2026-08-03 19:23:57 +0300

    feat(blog.ui.pages): add PageInfo to ExternalPage

--- src/test/java/ru/basted/basteduitests/blog/ui/pages/ExternalPage.java
+++ src/test/java/ru/basted/basteduitests/blog/ui/pages/ExternalPage.java
@@ -2,8 +2,10 @@ package ru.basted.basteduitests.blog.ui.pages;
 
 import org.openqa.selenium.WebDriver;
 
+import ru.basted.basteduitests.annotations.PageInfo;
 import ru.basted.basteduitests.base.AbstractPageCore;
 
+@PageInfo(siteEntity = "Внешний ресурс", name = "Внешняя страница")
 public final class ExternalPage extends AbstractPageCore<ExternalPage> {
     public ExternalPage(WebDriver webDriver) {
         super(webDriver);
